8fe027d9db95d22a6d6e06ff72e1b829ad9f1212
mxms-IP <mxms203@gmail.com>
2026-01-21 07:04:54 +0500

    components added

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [["module:react-native-dotenv"]],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
+
+
+
+
+
+
